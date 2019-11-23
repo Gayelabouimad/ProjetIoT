d9f55cdacbd71141c1f8fa021223a53a0eb6590a
Gayel Abou Imad <gayelabouimad@outlook.com>
2019-11-23 17:03:28 +0200

    server code added mqtt

--- Back-End/server.js
+++ Back-End/server.js
@@ -4,6 +4,34 @@ var express = require('express');
 // creating the app
 var app = express();
 
+
+// --------------------------------------
+var mqtt = require('mqtt')
+// Client Connection
+var client = mqtt.connect('http://212.98.137.194:1883', {"username": "user", "password": "bonjour"})
+
+client.on('connect', function () {
+    console.log("Connected")
+    // Client Subscription
+    client.subscribe('application/19/device/804a2bad98eef9b1/rx', function (err) {
+        console.log("Subscribed");
+        if(err){
+            console.log("error");
+        }
+    })
+})
+
+// When someone else publishes data
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log("Got a message", message.toString());
+    // let message2 = JSON.parse(message.toString());
+    client.end()
+  })
+// --------------------------------------
+
+
+
 // creating first route
 app.get('/', function(req, res){
    res.send("Hello From Main route");
