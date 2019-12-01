9fd555dfed5241532904bccbf2e546fa7434156c
Gayelabouimad <gayelabouimad@outlook.com>
2019-12-01 20:56:16 +0200

    homescreen almost done

--- IoTProject/screens/HomeScreen.js
+++ IoTProject/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import * as WebBrowser from 'expo-web-browser';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   Image,
   Platform,
@@ -11,54 +11,63 @@ import {
 } from 'react-native';
 import getClassrooms from '../services/APICalls';
 
+
+// [
+//   {"_id":"5dd64d081c9d440000b6e497","NumSalle":"SCI08","Date":"2018-12-31T22:00:00.000Z","NbHours":1.5,"Consumption":2.3},
+//   {"_id":"5de27f8e1c9d4400005140a7","NumSalle":"SCI18","Date":"2018-12-31T22:00:00.000Z","NbHours":1.5,"Consumption":2.3}
+// ]
+
 export default class HomeScreen extends Component {
   constructor() {
     super();
-    this.state = { 
-      classrooms: [] 
+    this.state = {
+      classrooms: []
     };
   }
-async componentWillMount(){
-  const classrooms = await getClassrooms();
-  console.log("----------",classrooms);
-
-  // if(classrooms){
-    this.setState({classrooms})
-
-  // }
-}
-render(){
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
+  async componentWillMount() {
+    const classrooms = await getClassrooms();
+    if (classrooms) {
+      this.setState({ classrooms })
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}>
           <View>
             <Text style={styles.Welcome}>
               Welcome to campus dashboard
             </Text>
           </View>
-          <View style={{margin: 10}}>
-            <Text>
+          <View style={{ margin: 10 }}>
+            <Text style={{fontSize: 20}}>
               The list below describes the state of each room on campus
             </Text>
             <View>
-            {this.state.classrooms.map((item, key) => {
-              return(
-              <Text key={key}>
-                {item["Name"]}
-              </Text>
-              );
-            })
-          }
-
+              {this.state.classrooms.map((item, key) => {
+                return (
+                  <View key={key} style={styles.row}>
+                    <Text style={styles.text}>
+                      {item["NumSalle"]} ({item["NbLampes"]} Lampes)
+                    </Text>
+                    {
+                      (item['isOn'])?
+                      <Text style={[styles.text ,{color: 'green'}]}>ON</Text>
+                      :
+                      <Text style={[styles.text ,{color: 'red'}]}>OFF</Text>
+                    }
+                  </View>
+                );
+              })
+              }
             </View>
           </View>
-      </ScrollView>
-    </View>
-  );
-}
-  
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 HomeScreen.navigationOptions = {
@@ -69,7 +78,17 @@ const styles = StyleSheet.create({
   Welcome: {
     fontSize: 25,
     margin: 20,
-    alignSelf:'center'
+    textAlign: 'center'
+  },
+  text: {
+    fontSize: 16
+  },
+  row: { 
+    flexDirection: "row", 
+    justifyContent: 'space-between', 
+    margin: 10,
+    padding: 5,
+    borderBottomColor: "#DCDCDC",
+    borderBottomWidth: 1
   }
-
 });
